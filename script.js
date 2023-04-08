54e03112b369972df27100a70793eb961b89f203
Wahdan Najmil <najmilwahdan@gmail.com>
2023-04-08 11:57:43 +0700

    add script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+let hasilbmi = document.querySelector("#hasil-bmi");
+let klasifikasihasil = document.querySelector("#klasifikasi-hasil");
+let tb = document.querySelector("#input-tb");
+let bb = document.querySelector("input-bb");
+
+// rumus : Berat Badan/(Tinggi Badan/100)^2
+// BMI Categories:
+// Underweight = <18.5
+// Normal weight = 18.5 – 24.9
+// Overweight = 25 – 29.9
+// Obesity = BMI of 30 or greater
+const tampilHasil = () => {
+    const bmi = bb.value / (tb.value * bb.value) ^ 2;
+
+    if (tb.value === '' || bb.value === '') {
+        hasilbmi.innerHTML = "Masukkan nilai yang valid."
+    } else if (bmi < 18.5) {
+        hasilbmi.innerHTML = `BMI Anda adalah ${bmi}.`;
+        klasifikasihasil.innerHTML = "Underweight(berat badan kurang)";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        hasilbmi.innerHTML = `BMI Anda adalah ${bmi}.`;
+        klasifikasihasil.innerHTML = "Normal weight(berat normal)";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        hasilbmi.innerHTML = `BMI Anda adalah ${bmi}.`;
+        klasifikasihasil.innerHTML = "Overweight(berat badan berlebih)";
+    } else {
+        klasifikasihasil.innerHTML = "Obesity(obesitas)";
+    }
+}
